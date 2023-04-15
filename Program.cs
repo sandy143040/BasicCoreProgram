399a32b0757fa1f1caf84478a933c47c00860f8c
Sandeep kumar singh <sandeeprj793@gmail.com>
2023-04-16 01:03:00 +0530

    [Sandeep] Finding year is Leap Year or not

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeapYear
+{
+    internal class Program
+    {
+        static bool checkYear(int year)
+        {
+            // If a year is multiple of 400, 
+            // then it is a leap year
+            if (year % 400 == 0)
+                return true;
+
+            // Else If a year is multiple of 100,
+            // then it is not a leap year
+            if (year % 100 == 0)
+                return false;
+
+            // Else If a year is multiple of 4,
+            // then it is a leap year
+            if (year % 4 == 0)
+                return true;
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a year: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.Write(checkYear(input) ? "Leap Year" :
+                                 "Not a Leap Year");
+            Console.ReadLine();
+        }
+    }
+}
