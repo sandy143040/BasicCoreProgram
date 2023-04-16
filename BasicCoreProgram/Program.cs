1895c4a547ad35ec89785d6b5787bd3b295a9771
Sandeep kumar singh <sandeeprj793@gmail.com>
2023-04-17 02:48:26 +0530

    [Sandeep] Modify Finding Quotient and Remainder

--- BasicCoreProgram/Program.cs
+++ BasicCoreProgram/Program.cs
@@ -8,28 +8,21 @@ namespace BasicCoreProgram
 {
     internal class Program
     {
-        public static void primeFactors(int n)
-        {
-            for (int i = 2; i * i <= n; i++)
-            {
-                while (n % i == 0)
-                {
-                    Console.Write("{0} ", i);
-                    n /= i;
-                }
-            }
-            if (n > 1)
-            {
-                Console.Write("{0} ", n);
-            }
-        }
         static void Main(string[] args)
         {
-            Console.Write("Enter a number to find its prime factors: ");
-            int n = Convert.ToInt32(Console.ReadLine());
-            
-            primeFactors(n);
-            
+            int dividend, divisor, quotient, remainder;
+
+            Console.WriteLine("Enter dividend: ");
+            dividend = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter divisor: ");
+            divisor = int.Parse(Console.ReadLine());
+
+            quotient = dividend / divisor;
+            remainder = dividend % divisor;
+
+            Console.WriteLine($"Quotient = {quotient}");
+            Console.WriteLine($"Remainder = {remainder}");
             Console.ReadLine();
         }
     }
