482ae15c474f52e839d5ab50b5ff5d48a0b1168b
Sandeep kumar singh <sandeeprj793@gmail.com>
2023-04-16 17:41:06 +0530

    [Sandeep] Welcome to Functional Problem

--- BasicCoreProgram/Program.cs
+++ BasicCoreProgram/Program.cs
@@ -10,6 +10,8 @@ namespace BasicCoreProgram
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Functional Programming");
+            Console.ReadLine();
         }
     }
 }
