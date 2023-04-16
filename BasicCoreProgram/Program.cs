db02657f0e6d1fe555929da224852dd479beb233
Sandeep kumar singh <sandeeprj793@gmail.com>
2023-04-17 04:00:52 +0530

    [Sandeep] Modify Swapping Two Numbers

--- BasicCoreProgram/Program.cs
+++ BasicCoreProgram/Program.cs
@@ -6,23 +6,44 @@ using System.Threading.Tasks;
 
 namespace BasicCoreProgram
 {
+    class SwapNumbers
+    {
+        private int num1;
+        private int num2;
+
+        public void Swap()
+        {
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
+
+        public void SetNumbers(int n1, int n2)
+        {
+            num1 = n1;
+            num2 = n2;
+        }
+
+        public void ShowNumbers()
+        {
+            Console.WriteLine($"After swapping: {num1}, {num2}");
+        }
+    }
     internal class Program
     {
+     
         static void Main(string[] args)
         {
-            int dividend, divisor, quotient, remainder;
+            Console.WriteLine("Enter first number: ");
+            int num1 = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter dividend: ");
-            dividend = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            int num2 = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter divisor: ");
-            divisor = int.Parse(Console.ReadLine());
-
-            quotient = dividend / divisor;
-            remainder = dividend % divisor;
-
-            Console.WriteLine($"Quotient = {quotient}");
-            Console.WriteLine($"Remainder = {remainder}");
+            SwapNumbers swap = new SwapNumbers();
+            swap.SetNumbers(num1, num2);
+            swap.Swap();
+            swap.ShowNumbers();
             Console.ReadLine();
         }
     }
