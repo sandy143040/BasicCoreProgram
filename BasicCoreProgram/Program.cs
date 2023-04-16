16c23fb0af7a77d93028393334715cd2a1d8dba5
Sandeep kumar singh <sandeeprj793@gmail.com>
2023-04-17 04:29:28 +0530

    [Sandeep] Modify checking given alphabet is vowel or consonant

--- BasicCoreProgram/Program.cs
+++ BasicCoreProgram/Program.cs
@@ -6,25 +6,23 @@ using System.Threading.Tasks;
 
 namespace BasicCoreProgram
 {
-    internal class EvenOrOddNumbers
-    {
-        public static string FindEvenOrOdd(int number)
-        {
-            if (number % 2 == 0)
-                return "Given number is even number";
-            else
-                return "Given number is odd number";
-        }
-    }
+   
     internal class Program
     {
-     
+        static void vowelOrConsonant(char x)
+        {
+            if (x == 'a' || x == 'e' || x == 'i' ||
+                              x == 'o' || x == 'u')
+                Console.WriteLine("Vowel");
+            else
+                Console.WriteLine("Consonant");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter integer number to find even or odd");
-            int input = Convert.ToInt32(Console.ReadLine());
-            string res = EvenOrOddNumbers.FindEvenOrOdd(input);
-            Console.WriteLine(res);
+            Console.WriteLine("Please Enter an alphabet");
+            char x = char.Parse(Console.ReadLine());
+            vowelOrConsonant(x);
             Console.ReadLine();
         }
     }
